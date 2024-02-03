80f3bef57800978bf4fcdc866f4e84a1a5ede010
Toxic_Blade <toxicblade.work@gmail.com>
2024-02-03 16:34:22 +0200

    config example added

---
+++ config_example.py
@@ -0,0 +1,9 @@
+BOTKEY='key from bot father'
+INFO_CHAT_ID='chat id, use get my id bot or smth like that'
+
+API_KEY='your gpt api key'
+
+OCR_API='your open space ocr api key '
+
+SMTP_MAIL='gmail'
+SMTP_PASS='app password'
