37201816ca81ca3ce2eff852d6a7aa0938ec9036
Toxic_Blade <toxicblade.work@gmail.com>
2024-02-06 19:26:48 +0200

    clear folder before adding new excels

--- src/write_excels.py
+++ src/write_excels.py
@@ -1,5 +1,7 @@
 import os
-DIRECTORY = "./data/"
+import patoolib
+
+DIRECTORY = "./data/excels"
 
 def get_excel_files(directory=DIRECTORY,delimiter='\n'):
     excel_files = []
@@ -15,3 +17,25 @@ def check_text_in_response(text_to_check, directory=DIRECTORY, delimiter='\n'):
 
 def format_excel_name(excel_name):
     return "./data/" + excel_name
+
+def clear_folder(folder_path = "./data/excels"):
+
+    # Получаем список файлов в папке
+    files = os.listdir(folder_path)
+    # Проходимся по всем файлам в папке
+    for file_name in files:
+        # Получаем полный путь к файлу
+        file_path = os.path.join(folder_path, file_name)
+        # Проверяем, является ли объект файлом
+        if os.path.isfile(file_path):
+            # Удаляем файл
+            os.remove(file_path)
+            print(f"Файл {file_path} удален.")
+        else:
+            print(f"Объект {file_path} не является файлом и не будет удален.")
+
+
+def extract_rar_to_excels(rar_file_path):
+    # Создаем папку для сохранения извлеченных файлов, если она не существует
+    patoolib.extract_archive(rar_file_path, outdir = './data/excels')
+    os.remove(rar_file_path)
