09021b154cb366b05f4b4a9caebc4f72e1ed9af3
Toxic_Blade <toxicblade.work@gmail.com>
2024-02-06 09:12:11 +0200

    post to create deal in crm with data

--- src/amo_crm_post.py
+++ src/amo_crm_post.py
@@ -75,13 +75,101 @@ def fetch_contacts(access_token, refresh_token, page=1, all_contacts=[]):
         print(f'Failed to fetch contacts: {response.text}')
         return None
 
+
+def create_deal_contact_company(access_token,refresh_token,data):
+
+    url = f'https://{SUBDOMAIN}.amocrm.ru/api/v4/leads/complex'
+    headers = get_headers(access_token)
+
+    params = {
+    "name": f'Deal for {data["name"]}',
+    "_embedded": {
+        "contacts": [
+            {
+                "first_name": data["name"],
+                "responsible_user_id": 9805834,
+                "updated_by": 0,
+                "custom_fields_values": [
+                    {
+                        "field_id": 8187,
+                        "values": [
+                            {
+                                "value": data["description"],
+                            }
+                        ],
+                    }, {
+                "field_id": 8191,
+                "values": [
+                    {
+                        "value": data["email"]
+                    }
+                ]
+            },
+            {
+                "field_id": 8189,
+                "values": [
+                    {
+                        "value": data["phone_number"]
+                    }
+                ]
+            }
+                ],
+            }
+        ],
+        "companies": [
+            {
+                "name": data["company_name"],
+                "custom_fields_values": [
+                    {
+                        "field_id": 8197,
+                        "values": [
+                            {
+                                "value": data["address"],
+                            }
+                        ],
+                    },
+                ],
+            }
+        ]
+    },
+    "responsible_user_id": 9805834,
+    "pipeline_id": 7213102,
+}
+
+    response = session.post(url, json=[params], headers=headers)
+    if response.status_code == 200:
+        print('Deal created successfully')
+        print(response)
+    elif response.status_code == 401:
+        new_tokens = refresh_access_token(refresh_token)
+        if new_tokens:
+            create_deal_contact_company(new_tokens['access_token'], new_tokens['refresh_token'], data)
+        else:
+            print('Failed to refresh token and create a deal')
+    else:
+        print(f'Failed to create a deal: {response.text}')
 # Example usage
 if __name__ == "__main__":
     ACCESS_TOKEN = os.getenv('AMOCRM_ACCESS_TOKEN')
     REFRESH_TOKEN = os.getenv('AMOCRM_REFRESH_TOKEN')
 
-    contacts = fetch_contacts(ACCESS_TOKEN, REFRESH_TOKEN)
-    if contacts is not None:
-        print(f'Fetched {len(contacts)} contacts')
-    else:
-        print('No contacts fetched')
+    data = {
+        "name": "ZAFER PALABIYIK",
+        "email": "zafer.palabiyik@ozsofra.com.mt",
+        "phone_number": "+356 9987 4001",
+        "company_name": "Ozsofra Group",
+        "address": "Triq il-Korp tal-Pijunieri, Bugibba, Malta",
+        "description": "Managing Director"
+    }
+
+    print(data['address'])
+
+    create_deal_contact_company(ACCESS_TOKEN, REFRESH_TOKEN, data)
+
+
+
+    # contacts = fetch_contacts(ACCESS_TOKEN, REFRESH_TOKEN)
+    # if contacts is not None:
+    #     print(f'Fetched {len(contacts)} contacts')
+    # else:
+    #     print('No contacts fetched')
