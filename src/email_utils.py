930ce2aa91c7f20a8aae78273faa5f36e8a4f5e2
Toxic_Blade <toxicblade.work@gmail.com>
2024-02-12 23:07:07 +0200

    now always sends catalog, and you can add

--- src/email_utils.py
+++ src/email_utils.py
@@ -8,6 +8,8 @@ import re
 import json
 from dotenv import load_dotenv
 
+PREDEFINED_PDF_FILENAME = "./data/sam_global_catalog.pdf"
+
 load_dotenv()
 
 SMTP_MAIL = os.getenv("SMTP_MAIL")
@@ -53,7 +55,7 @@ def extract_and_replace_from_json(json_string, json_file_path=JSON_FILE_PATH):
         "missing_placeholders": list(set(placeholders) - {"client_name", "product_name", "company_name"})
     }
 
-def send_email(bot,json_text,receiver_email, attachment_path=None):
+def send_email(bot,json_text,receiver_email, attachment_path_1=PREDEFINED_PDF_FILENAME, attachment_path_2 = None):
     """
     Connecting to gmail smtp using email and app password
     Adding attachment if provided
@@ -69,18 +71,27 @@ def send_email(bot,json_text,receiver_email, attachment_path=None):
         msg.attach(MIMEText(data.get("replaced_text","")))
 
         # Attach Excel file if provided
-        if attachment_path:
-            with open(attachment_path, 'rb') as attachment:
-                part = MIMEBase('application', 'octet-stream')
-                part.set_payload(attachment.read())
-                encoders.encode_base64(part)
-                part.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(attachment_path)}"')
-                msg.attach(part)
+        with open(attachment_path_1, 'rb') as attachment_1:
+            part_1 = MIMEBase('application', 'octet-stream')
+            part_1.set_payload(attachment_1.read())
+            encoders.encode_base64(part_1)
+            part_1.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(attachment_path_1)}"')
+            msg.attach(part_1)
+
+        # Attach the second file
+        if attachment_path_2:
+            with open(attachment_path_2, 'rb') as attachment_2:
+                part_2 = MIMEBase('application', 'octet-stream')
+                part_2.set_payload(attachment_2.read())
+                encoders.encode_base64(part_2)
+                part_2.add_header('Content-Disposition', f'attachment; filename="{os.path.basename(attachment_path_2)}"')
+                msg.attach(part_2)
 
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
             server.login(SMTP_USERNAME, SMTP_PASSWORD)
             server.sendmail(SMTP_USERNAME, receiver_email, msg.as_string())
+
     except smtplib.SMTPAuthenticationError as e:
         # Handle the case where credentials are outdated
         bot.send_message(chat_id=CHAT_TEST, text='Please update your credentials of OCR BOT.He cant send emails at the moment')
