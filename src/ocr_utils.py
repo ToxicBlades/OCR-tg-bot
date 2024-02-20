6ae52cd58958b0b5fb01b20889ac817c07ffd24f
Toxic_Blade <toxicblade.work@gmail.com>
2024-02-20 13:26:04 +0200

    preprocess with tinify api

--- src/ocr_utils.py
+++ src/ocr_utils.py
@@ -9,46 +9,17 @@ from pillow_heif import register_heif_opener
 
 register_heif_opener() # to detect if its heic
 
+import tinify
+
+
 load_dotenv()
 OCR_API = os.getenv("OCR_API")
+TINIFY_API_KEY = os.getenv("TINIFY_API_KEY")
+
+tinify.key = TINIFY_API_KEY
+
 
 MAX_FILE_SIZE_MB = 1 * 1024 * 1024  # 1MB in Bytes
-
-def compress_image(image_path, output_path=None, quality=35):
-    """
-    Compresses an image or converts it from HEIC to JPEG if necessary, then reduces its file size to be less than 1 MB.
-    """
-    # Open the image using Pillow, which now supports HEIC through Pillow-Heif
-    img = Image.open(image_path)
-    img_format = 'JPEG'  # We default to JPEG for output
-
-    # If an output path wasn't specified, generate one based on the input path
-    if not output_path:
-        base, _ = os.path.splitext(image_path)
-        output_path = f"{base}.jpeg"
-
-    img_bytes = BytesIO()
-    img.save(img_bytes, format=img_format, quality=quality, optimize=True)
-
-    # More aggressive quality reduction and resizing if necessary
-    while img_bytes.tell() > MAX_FILE_SIZE_MB:
-        img_bytes.seek(0)  # Reset the BytesIO object
-        quality -= 10  # Decrease quality in larger steps
-        if quality <= 20:
-            # If quality is already low, start resizing
-            scale_factor = (MAX_FILE_SIZE_MB / img_bytes.tell()) ** 0.5
-            new_width = int(img.width * scale_factor)
-            new_height = int(img.height * scale_factor)
-            img = img.resize((new_width, new_height), Image.ANTIALIAS)
-            quality = 75  # Reset quality for resized image
-        img.save(img_bytes, format=img_format, quality=quality, optimize=True)
-
-    img_bytes.seek(0)  # Go back to the start of the BytesIO object
-    with open(output_path, 'wb') as f:
-        f.write(img_bytes.getvalue())  # Write the final compressed image to disk
-
-    return output_path
-
 
 def ocr_space_file(filename, overlay=False, api_key=OCR_API, language='eng'):
     """
@@ -60,7 +31,9 @@ def ocr_space_file(filename, overlay=False, api_key=OCR_API, language='eng'):
 
     if os.path.getsize(filename) > MAX_FILE_SIZE_MB:
         print(f"Original file size is larger than 1MB, compressing {filename}...")
-        filename = compress_image(filename)
+
+        source = tinify.from_file(filename)
+        source.to_file(filename)
 
 
 
@@ -91,3 +64,9 @@ def extract_email_from_ocr_result(ocr_result):
         return match.group()
     else:
         return None
+
+
+
+
+
+# print(ocr_space_file('./src/test.JPG'))
